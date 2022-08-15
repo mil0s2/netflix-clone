647b97df11065c00a0e4dafb88724caa72499818
mil0s2 <miloszmazurk@gmail.com>
2022-08-15 17:52:35 +0200

    Front page banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,11 +1,52 @@
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 
-const Banner = () => {
+import { baseUrl } from '../constants/movie';
+import { Movie } from '../typings';
+import { PlayIcon } from '@heroicons/react/solid';
+import { InformationCircleIcon } from '@heroicons/react/outline';
+
+interface Props {
+  netflixOriginals: Movie[];
+}
+
+const Banner = ({ netflixOriginals }: Props) => {
+  const [movie, setMovie] = useState<Movie | null>();
+
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    );
+  }, []);
+
+  console.log(movie);
+
   return (
-    <div>
-      {/* <div>
-        <Image />
-      </div> */}
+    <div className="flex flex-col gap-2 py-16 md:gap-4 lg:h-[75vh] lg:justify-end lg:pb-12">
+      <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          alt={`${movie?.title} movie poster`}
+          objectFit="cover"
+          layout="fill"
+        />
+      </div>
+      <h1 className="text-shadow-md text-2xl font-semibold lg:text-7xl">
+        {movie?.title || movie?.name || movie?.original_name}
+      </h1>
+      <p className="lg:font-2xl text-shadow-md max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl">
+        {movie?.overview}
+      </p>
+
+      <div className="flex gap-3">
+        <button className="bannerButton bg-white text-black">
+          <PlayIcon className="-ml-3 h-4 w-4 text-black md:h-7 md:w-7" /> Play
+        </button>
+        <button className="bannerButton bg-[gray]/70">
+          <InformationCircleIcon className="-ml-1 h-4 w-4 md:h-7 md:w-7" /> More
+          Info
+        </button>
+      </div>
     </div>
   );
 };
