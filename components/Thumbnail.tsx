f5804189846a4af18eee48cf9e83c205429cdc16
mil0s2 <miloszmazurk@gmail.com>
2022-08-24 21:02:37 +0200

    player

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import { useRecoilState } from 'recoil';
+import { modalState, movieState } from '../atoms/modalAtom';
 
 import { Movie } from '../typings';
 
@@ -8,8 +10,18 @@ interface Props {
 }
 
 const Thumbnail = ({ movie }: Props) => {
+  const [_, setShowModal] = useRecoilState(modalState);
+  const [currentMovie, setCurrentMovie] = useRecoilState(movieState);
+  console.log(movie);
+
   return (
-    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
+    <div
+      onClick={() => {
+        setShowModal(true);
+        setCurrentMovie(movie);
+      }}
+      className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105"
+    >
       <Image
         src={`https://image.tmdb.org/t/p/w500${
           movie.backdrop_path || movie.poster_path
@@ -17,6 +29,9 @@ const Thumbnail = ({ movie }: Props) => {
         className="rounded-sm object-cover md:rounded"
         layout="fill"
       />
+      <h6 className="text-shadow-md absolute bottom-2 left-1/2 w-full -translate-x-1/2 transform px-2 text-lg font-semibold">
+        {movie.title || movie.name}
+      </h6>
     </div>
   );
 };
